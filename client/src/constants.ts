8e7745e444960f9c93d518ab9c3665b4e357d648
meraeugene <villalon.andrew123@gmail.com>
2024-11-05 20:57:11 +0800

    change constants

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://fars-v1.onrender.com/";
 export const UPLOADS_URL = "/api/upload";
 export const REVIEWS_URL = "/api/reviews";
 export const ADMIN_URL = "/api/admin";
