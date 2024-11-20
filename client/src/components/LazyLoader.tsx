965eb961afb6197b10e57a3a32408aff9a9b9ebf
meraeugene <villalon.andrew123@gmail.com>
2024-11-20 09:29:11 +0800

    improve lazy loading

--- client/src/components/LazyLoader.tsx
+++ client/src/components/LazyLoader.tsx
@@ -1,12 +1,16 @@
 const LazyLoader = () => {
   return (
-    <div className={`mt-8 flex h-screen w-full items-center justify-center`}>
-      <l-line-spinner
+    <div
+      className={`flex h-screen w-full flex-col items-center justify-center gap-4 bg-[#f0f4ff]`}
+    >
+      {/* <l-line-spinner
         size="30"
         stroke="3"
         speed="1"
         color="#3b82f6"
-      ></l-line-spinner>{" "}
+      ></l-line-spinner>{" "} */}
+      <div className="loader"></div>
+      <h1 className="font-semibold text-[#0c1b4d]">FARS</h1>
     </div>
   );
 };
